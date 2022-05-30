2b26016242079837a01ac9b50c5e1159f62d984e
oliver <kunhan945@gmail.com>
2022-05-30 11:27:00 +0800

    funders function

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,8 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-	// storage variables
-	uint public funds = 1000;
-	int public counter = -10;
-	uint32 public test = 4294967295;
+
+	address[] public funders;
+
+	receive() external payable {}
+
+	function addFunds() external payable {
+		funders.push(msg.sender);
+	}
+
+	function getAllFunders() external view returns (address[] memory) {
+		return funders;
+	}
 }
